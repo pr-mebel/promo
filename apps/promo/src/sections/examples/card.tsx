ec18e206e84b6cf138298f2440a4f1f618a0cc77
Nikita Yakunin <nayakunin99@gmail.com>
2024-10-24 19:34:45 +0200

    implement examples

--- apps/promo/src/sections/examples/card.tsx
+++ apps/promo/src/sections/examples/card.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import {
@@ -7,6 +8,10 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { ArrowRight } from "../../icons";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { X } from "lucide-react";
 
 type CardProps = {
   title: string;
@@ -23,37 +28,78 @@ export const Card = ({ images, main, title }: CardProps) => {
         }
       }}
     >
-      <AlertDialogTrigger>
+      <AlertDialogTrigger className="md:relative md:col-span-4">
         <div className="flex cursor-pointer flex-col items-center">
           <img
             src={main}
             alt={title}
-            className="aspect-[398/360] w-full object-cover"
+            className="aspect-[398/360] w-full object-cover md:aspect-[3/2]"
           />
-          <div className="mt-2 flex w-full items-start justify-between">
+          {/* mobile only */}
+          <div className="mt-2 flex w-full items-start justify-between md:hidden">
             <div className="flex gap-3">
               <h4 className="text-xl font-medium uppercase">{title}</h4>
               <p className="text-xl text-dark-700">{images.length} фото</p>
             </div>
             <ArrowRight className="h-7 w-7" />
           </div>
+          {/* desktop only */}
+          <div className="absolute hidden h-full w-full flex-col items-center text-white opacity-0 transition-opacity duration-200 hover:opacity-100 hover:backdrop-blur-md hover:backdrop-brightness-50 md:flex">
+            <h4 className="mb-3 mt-[15%] text-lg font-medium uppercase">
+              {title}
+            </h4>
+            <p className="mb-4 text-lg">{images.length} фото</p>
+            <div
+              className={cn(
+                buttonVariants({ variant: "accent", size: "small" }),
+                "mx-auto",
+              )}
+            >
+              Смотреть все фото
+            </div>
+          </div>
         </div>
       </AlertDialogTrigger>
-      <AlertDialogContent className="">
-        <div className="pointer-events-auto left-0 top-0 h-screen w-full overflow-scroll px-6 py-4">
+      <AlertDialogContent>
+        {/* mobile */}
+        <div className="inset-0 h-screen w-full overflow-scroll px-6 py-4 md:hidden">
           <div className="relative flex flex-col gap-4 pb-12">
-            {images.map((src) => (
-              <img
-                key={src}
-                src={src}
-                alt={title}
-                className="w-full object-cover"
-              />
+            {images.map((src, i) => (
+              <div className="relative aspect-square">
+                <Image
+                  key={src}
+                  src={src}
+                  alt={title}
+                  fill
+                  priority={i < 3}
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
             ))}
             <AlertDialogCancel className="fixed bottom-0 left-0 w-full">
               Назад
             </AlertDialogCancel>
           </div>
+        </div>
+        {/* desktop */}
+        <div className="relative inset-0 hidden h-screen w-full overflow-scroll px-6 py-4 md:block">
+          <div className="grid grid-cols-12 gap-4">
+            {images.map((src, i) => (
+              <div className="relative col-span-4 aspect-square">
+                <Image
+                  key={src}
+                  src={src}
+                  alt={title}
+                  fill
+                  priority={i < 3}
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
+            ))}
+          </div>
+          <AlertDialogCancel className="fixed right-12 top-6">
+            <X />
+          </AlertDialogCancel>
         </div>
       </AlertDialogContent>
     </AlertDialog>
--- apps/promo/src/sections/examples/index.tsx
+++ apps/promo/src/sections/examples/index.tsx
@@ -5,19 +5,21 @@ import { getCards } from "./cards";
 export const ExamplesSection = async () => {
   const cards = await getCards();
   return (
-    <section className="container pb-10">
+    <section className="py-10 max-md:container">
       <h3 className="uppercase text-dark-700">Портфолио</h3>
       <h2 className="mb-4 text-3xl font-medium">
         Посмотрите на примеры выполненных нами работ
       </h2>
-      <div className="mb-8 flex flex-col gap-6">
+      <div className="mb-8 flex flex-col gap-6 md:grid md:grid-cols-12 md:gap-0">
         {cards.map((card, index) => (
           <Card key={index} {...card} />
         ))}
       </div>
-      <Button className="w-full" variant="accent">
-        Узнать стоимость
-      </Button>
+      <div className="md:flex md:justify-center">
+        <Button className="max-md:w-full" variant="accent">
+          Узнать стоимость
+        </Button>
+      </div>
     </section>
   );
 };
