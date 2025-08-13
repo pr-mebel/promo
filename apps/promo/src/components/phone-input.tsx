06035a2faab13d9ebda24d57d442e6d1729c5608
Nikita Yakunin <nayakunin99@gmail.com>
2025-08-13 22:50:34 +0200

    fixes

--- apps/promo/src/components/phone-input.tsx
+++ apps/promo/src/components/phone-input.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import InputMask from "react-input-mask";
+"use client";
+import React, { useState, useCallback } from "react";
 import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
 
@@ -7,19 +7,87 @@ interface PhoneInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
 }
 
+const removeLetters = (value: string) => {
+  return value.replace(/\D/g, "");
+};
+
+export const formatPhoneInput = (rawValue: string) => {
+  let value = removeLetters(rawValue);
+  let result = "";
+
+  if (!value) {
+    return "";
+  }
+
+  if (value[0] !== "8" && value[0] !== "7") {
+    value = `7${value}`;
+  }
+
+  // Convert 8 to 7 for consistency
+  if (value[0] === "8") {
+    value = `7${value.substring(1)}`;
+  }
+
+  result = `+7`;
+
+  if (value.length > 1) {
+    result = `${result} (${value.substring(1, 4)}`;
+  }
+
+  if (value.length > 4) {
+    result = `${result}) ${value.substring(4, 7)}`;
+  }
+
+  if (value.length > 7) {
+    result = `${result}-${value.substring(7, 9)}`;
+  }
+
+  if (value.length > 9) {
+    result = `${result}-${value.substring(9, 11)}`;
+  }
+
+  return result;
+};
+
 export const PhoneInput = React.forwardRef<HTMLInputElement, PhoneInputProps>(
-  ({ className, formAction: _, ...props }, ref) => {
+  ({ className, onChange, value, ...props }, ref) => {
+    const [displayValue, setDisplayValue] = useState(() =>
+      value ? formatPhoneInput(String(value)) : "",
+    );
+
+    const handleChange = useCallback(
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue = e.target.value;
+        const formatted = formatPhoneInput(inputValue);
+
+        setDisplayValue(formatted);
+
+        if (onChange) {
+          const cleanValue = removeLetters(formatted);
+          const syntheticEvent = {
+            ...e,
+            target: {
+              ...e.target,
+              value: cleanValue,
+              name: props.name || "",
+            },
+          };
+          onChange(syntheticEvent);
+        }
+      },
+      [onChange, props.name],
+    );
+
     return (
-      <InputMask mask="+7 (999) 999-99-99" maskChar="_" {...props}>
-        {(inputProps: any) => (
-          <Input
-            {...inputProps}
-            ref={ref}
-            type="tel"
-            className={cn(className)}
-          />
-        )}
-      </InputMask>
+      <Input
+        {...props}
+        ref={ref}
+        type="tel"
+        value={displayValue}
+        onChange={handleChange}
+        placeholder={props.placeholder || "+7 (___) ___-__-__"}
+        className={cn(className)}
+      />
     );
   },
 );
